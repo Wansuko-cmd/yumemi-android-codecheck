cb2ab14480ab5bea2e574223570c12b1dfbba2ac
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-10 00:27:27 +0900

    [fix] showを括りつけ

--- android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/show/ShowFragment.kt
+++ android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/show/ShowFragment.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.codecheck
+package jp.co.yumemi.android.codecheck.show
 
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +9,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
+import jp.co.yumemi.android.codecheck.R
 import jp.co.yumemi.android.codecheck.TopActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.codecheck.databinding.FragmentShowBinding
 
