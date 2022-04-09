67cb28dd34f40b34954e1ed5e4f7ff18046f43bd
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-09 16:35:53 +0900

    [add] Repositoryドメインを追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/domain/Repository.kt
@@ -0,0 +1,32 @@
+package jp.co.yumemi.android.codecheck.domain
+
+data class Repository(
+    val name: Name,
+    val ownerIconUrl: OwnerIconUrl,
+    val language: Language,
+    val stargazersCount: StargazersCount,
+    val watchersCount: WatchersCount,
+    val forksCount: ForksCount,
+    val openIssuesCount: OpenIssuesCount,
+)
+
+@JvmInline
+value class Name(val value: String)
+
+@JvmInline
+value class OwnerIconUrl(val value: String)
+
+@JvmInline
+value class Language(val value: String)
+
+@JvmInline
+value class StargazersCount(val value: Long)
+
+@JvmInline
+value class WatchersCount(val value: Long)
+
+@JvmInline
+value class ForksCount(val value: Long)
+
+@JvmInline
+value class OpenIssuesCount(val value: Long)
