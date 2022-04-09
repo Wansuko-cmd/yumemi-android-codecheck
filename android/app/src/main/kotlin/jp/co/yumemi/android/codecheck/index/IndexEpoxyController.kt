960d925bff198ad2effd999f12cd278c3c2805b4
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-10 00:21:43 +0900

    [add] Rowをタッチしたときのイベントを追加

--- android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/index/IndexEpoxyController.kt
+++ android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/index/IndexEpoxyController.kt
@@ -4,12 +4,15 @@ import com.airbnb.epoxy.TypedEpoxyController
 import jp.co.yumemi.android.codecheck.indexGithubRepoRow
 import jp.co.yumemi.android.codecheck.utils.GithubRepoUiState
 
-class IndexEpoxyController : TypedEpoxyController<List<GithubRepoUiState>>() {
+class IndexEpoxyController(
+    private val onClickRow: (GithubRepoUiState) -> Unit,
+) : TypedEpoxyController<List<GithubRepoUiState>>() {
     override fun buildModels(githubRepos: List<GithubRepoUiState>) {
         githubRepos.forEach { githubRepo ->
             indexGithubRepoRow {
                 id(this@IndexEpoxyController.modelCountBuiltSoFar)
                 name(githubRepo.name)
+                onClickRow { _ -> this@IndexEpoxyController.onClickRow(githubRepo) }
             }
         }
     }
