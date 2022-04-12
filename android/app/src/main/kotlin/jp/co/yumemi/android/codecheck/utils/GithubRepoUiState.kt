9c76f7687fdba12f111fc4c78aff7aca87f67ed7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-12 22:40:57 +0900

    [fix] GithubRepoUiStateの持つ情報の型を全てStringに変更

--- android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/utils/GithubRepoUiState.kt
+++ android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/utils/GithubRepoUiState.kt
@@ -9,20 +9,20 @@ data class GithubRepoUiState(
     val name: String,
     val ownerIconUrl: String,
     val language: String,
-    val stargazersCount: Long,
-    val watchersCount: Long,
-    val forksCount: Long,
-    val openIssuesCount: Long,
+    val stargazersCount: String,
+    val watchersCount: String,
+    val forksCount: String,
+    val openIssuesCount: String,
 ) : Parcelable {
     companion object {
         fun GithubRepoUseCaseModel.toUiState() = GithubRepoUiState(
             name = name,
             ownerIconUrl = ownerIconUrl,
             language = language,
-            stargazersCount = stargazersCount,
-            watchersCount = watchersCount,
-            forksCount = forksCount,
-            openIssuesCount = openIssuesCount,
+            stargazersCount = stargazersCount.toString(),
+            watchersCount = watchersCount.toString(),
+            forksCount = forksCount.toString(),
+            openIssuesCount = openIssuesCount.toString(),
         )
     }
 }
