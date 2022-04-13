f7b363a2fbdcfef0e1dbc52ebeba124ae7b1442b
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-13 23:36:55 +0900

    [add] usecaseで利用するテストのライブラリを追加

--- core/usecase/build.gradle.kts
+++ core/usecase/build.gradle.kts
@@ -1,4 +1,9 @@
 dependencies {
     implementation(project(":core:domain"))
     implementation(project(":utils"))
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("io.mockk:mockk:1.12.3")
+    testImplementation("com.google.truth:truth:1.1.3")
 }
