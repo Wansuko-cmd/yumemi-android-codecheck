e3818301b6668366d3f5ff0057e0e11815018657
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-13 17:50:05 +0900

    [fix] navigationをするということがわかりやすい命名に変更

--- android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/index/IndexFragment.kt
+++ android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/index/IndexFragment.kt
@@ -39,7 +39,7 @@ class IndexFragment : Fragment(R.layout.fragment_index) {
                 return@setOnEditorActionListener false
             }
 
-        val indexEpoxyController = IndexEpoxyController(::gotoRepositoryFragment)
+        val indexEpoxyController = IndexEpoxyController(::navigateToShow)
 
         binding.indexRecyclerView.apply {
             addItemDecoration(
@@ -62,7 +62,7 @@ class IndexFragment : Fragment(R.layout.fragment_index) {
         }
     }
 
-    fun gotoRepositoryFragment(githubRepo: GithubRepoUiState) {
+    fun navigateToShow(githubRepo: GithubRepoUiState) {
         val action = IndexFragmentDirections
             .actionRepositoriesFragmentToRepositoryFragment(githubRepo = githubRepo)
         findNavController().navigate(action)
