aa7e16f6c158c1ff6ba7b3b609371f6fcd0db1e7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-14 19:48:50 +0900

    [fix] SharedFlowに変更

--- android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/index/IndexUiState.kt
+++ android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/index/IndexUiState.kt
@@ -5,10 +5,6 @@ import jp.co.yumemi.android.codecheck.utils.State
 
 data class IndexUiState(
     val githubRepos: State<List<GithubRepoUiState>, IndexErrorUiState> = State.Loading,
-) {
-    fun copyWithGithubRepos(
-        githubRepos: State<List<GithubRepoUiState>, IndexErrorUiState>,
-    ) = this.copy(githubRepos = githubRepos)
-}
+)
 
 data class IndexErrorUiState(val message: String)
--- android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/index/IndexViewModel.kt
+++ android/app/src/main/kotlin/jp/co/yumemi/android/codecheck/index/IndexViewModel.kt
@@ -12,7 +12,9 @@ import jp.co.yumemi.android.codecheck.SearchGithubReposUseCaseException
 import jp.co.yumemi.android.codecheck.mapBoth
 import jp.co.yumemi.android.codecheck.utils.GithubRepoUiState.Companion.toUiState
 import jp.co.yumemi.android.codecheck.utils.asState
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -21,8 +23,8 @@ class IndexViewModel(
     private val application: Application,
     private val searchGithubUseCase: SearchGithubReposUseCase,
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow(IndexUiState())
-    val uiState get() = _uiState.asStateFlow()
+    private val _uiState = MutableSharedFlow<IndexUiState>(replay = 1)
+    val uiState get() = _uiState.asSharedFlow()
 
     fun fetch(queryString: String) {
         viewModelScope.launch { _uiState.emit(IndexUiState()) }
@@ -45,7 +47,7 @@ class IndexViewModel(
                     }
                 )
                 .asState()
-                .also { githubRepos -> _uiState.update { it.copyWithGithubRepos(githubRepos) } }
+                .also { githubRepos -> _uiState.emit(IndexUiState(githubRepos)) }
         }
     }
 }
